8fbfaf3fa7ce4b4fbee828c90eb798d25c177a5a
kurotkin <kurotkin@users.noreply.github.com>
2019-09-19 22:36:43 +0300

    Второй вариант анемированного лоадера

--- app/src/main/java/com/kurotkin/directlotapp/LoaderView.kt
+++ app/src/main/java/com/kurotkin/directlotapp/LoaderView.kt
@@ -10,20 +10,26 @@ import android.view.View
 
 class LoaderView(context: Context, attributeSet: AttributeSet): View(context, attributeSet){
 
-    val blueColor = Color.parseColor("#58C1F6")
-    val pinkColor = Color.parseColor("#580011")
+    val blueColor = Color.parseColor("#FF0000")
+    val pinkColor = Color.parseColor("#0000FF")
 
     val rect = RectF()
     val paint = Paint().apply {
         color = blueColor
     }
+    val dotPaint = Paint().apply {
+        color = Color.RED
+        strokeWidth = 2f
+    }
+
     val size = resources.displayMetrics.density * 100
     val halfSize = size / 2
 
     var rad = 0f
     var rot = 0f
     var color = blueColor
-    val bitmap = BitmapFactory.decodeResource(resources, R.drawable.android)
+    val bitmap = BitmapFactory.decodeResource(resources, R.drawable.logodl)
+    var radLittle = 1f
 
     var dur = 2000
     init {
@@ -44,12 +50,28 @@ class LoaderView(context: Context, attributeSet: AttributeSet): View(context, at
         if(canvas == null) return
         paint.color = color
         canvas.rotate(rot, (width / 2).toFloat(), (height / 2).toFloat())
-        canvas.drawRoundRect(rect, rad, rad, paint)
+//        canvas.drawRoundRect(rect, rad, rad, paint)
 
         val matrix = Matrix()
         matrix.postScale(0.1f, 0.1f)
 //        matrix.postRotate(45f)
-        canvas.drawBitmap(bitmap, (width / 2).toFloat(), (height / 2).toFloat(), paint)
+        canvas.drawBitmap(bitmap, ((width - bitmap.width) / 2 ).toFloat() , ((height  - bitmap.height) / 2).toFloat(), paint)
+        val radC = 90f
+        val w = width.toFloat()
+        val h = height.toFloat()
+        canvas.drawCircle(w / 2f + 0.0f * radC, h / 2f - 1.0f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f + 0.5f * radC, h / 2f - 0.866f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f + 0.866f * radC, h / 2f - 0.5f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f + 1.0f * radC, h / 2f - 0.0f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f + 0.866f * radC, h / 2f + 0.5f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f + 0.5f * radC, h / 2f + 0.866f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f - 0.0f * radC, h / 2f + 1.0f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f - 0.5f * radC, h / 2f + 0.866f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f - 0.866f * radC, h / 2f + 0.5f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f - 1.0f * radC, h / 2f + 0.0f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f - 0.866f * radC, h / 2f - 0.5f * radC, radLittle, paint)
+        canvas.drawCircle(w / 2f - 0.5f * radC, h / 2f - 0.866f * radC, radLittle, paint)
+
     }
 
     fun startAnimation(){
@@ -63,12 +85,14 @@ class LoaderView(context: Context, attributeSet: AttributeSet): View(context, at
 //        vAnim.start()
 
         val ra = PropertyValuesHolder.ofFloat("rad", 0f, 150f)
+        val rProp = PropertyValuesHolder.ofFloat("radL", 1f, 100f)
         val ro = PropertyValuesHolder.ofFloat("rot", 0f, 360f)
         val co = PropertyValuesHolder.ofObject("color", ArgbEvaluator(), blueColor, pinkColor)
-        val vAnim2 = ValueAnimator.ofPropertyValuesHolder(ra, ro, co).apply {
+        val vAnim2 = ValueAnimator.ofPropertyValuesHolder(ra, rProp, ro, co).apply {
             duration = this@LoaderView.dur.toLong()
             addUpdateListener {
                 rad = it.getAnimatedValue("rad") as Float
+                radLittle = it.getAnimatedValue("radL") as Float
                 rot = it.getAnimatedValue("rot") as Float
                 color = it.getAnimatedValue("color") as Int
                 invalidate()
