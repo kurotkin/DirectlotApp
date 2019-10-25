147eb36d1eefd7fe709abdd47ac547d13218d51f
Vitaly Kurotkin <example@example.com>
2019-10-25 12:26:53 +0300

    Добавлен презентер

---
+++ app/src/main/java/com/kurotkin/directlotapp/presenter/LotListPresenter.kt
@@ -0,0 +1,18 @@
+package com.kurotkin.directlotapp.presenter
+
+class LotListPresenter : Presenter() {
+
+    override fun onMakeList() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onRefreshList() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onOpenLot(id: Long) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
